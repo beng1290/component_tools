9fbbf63248236a64a9a595aa91328f9a8c466991
Ben Green <46007058+beng1290@users.noreply.github.com>
2023-04-03 10:22:57 -0400

    Update add_color.m

--- image_figure_1_multiplex/add_color.m
+++ image_figure_1_multiplex/add_color.m
@@ -7,7 +7,7 @@ function im2 = add_color(im, mycol, color_scale, vals, vals2)
 %
 a = 180;
 b = 2;
-im2 = a * asinh(b * im) * mycol.notumor;
+im2 = a * asinh(b * im) * mycol.notumorfoxp3;
 im2 = reshape(im2,1004,1344,3);
 %
 a = 250;
@@ -16,5 +16,12 @@ im3 = a * asinh(b * im) * mycol.tumor;
 im3 = reshape(im3,1004,1344,3);
 %
 im2 = im2 + im3;
-
+%
+a3 = 180;
+b3 = 2;
+im4 = a3 * asinh(b3 * im) * mycol.foxp3;
+im4 = reshape(im4,1004,1344,3);
+%
+im4 = im2 + im4;
+%
 end
